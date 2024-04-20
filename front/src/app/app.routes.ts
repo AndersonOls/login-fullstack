4b1033e33b2b0b3db394dc65a4e2b910615021a7
Anderson <anderson.silva1996@hotmail.com>
2024-04-20 10:53:42 -0300

    feat: default layout style

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -2,8 +2,6 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
-    {
-        path: 'login',
-        component: LoginComponent
-    }
+    {'path': '', component:LoginComponent},
+    {path: 'login', component: LoginComponent}
 ];
